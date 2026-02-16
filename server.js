92461ca253ea6130bf0d9e47f91a9724bc1089d7
Bruno Fischer <brunoalmeidafischer@gmail.com>
2026-02-16 11:54:08 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,56 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import { prisma } from "./lib/prisma.ts";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post("/users", async (req, res) => {
+  await prisma.user.create({
+    data: {
+      name: req.body.name,
+      age: req.body.age,
+    },
+  });
+  res.status(201).json({ message: "User created successfully" });
+});
+
+app.put("/users/:id", async (req, res) => {
+  await prisma.user.update({
+    where: {
+      id: parseInt(req.params.id),
+    },
+    data: {
+      name: req.body.name,
+      age: req.body.age,
+    },
+  });
+  res.status(201).json({ message: "User updated successfully" });
+});
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.status(200).json(users);
+});
+
+app.get("/users/:id", async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: parseInt(req.params.id),
+    },
+  });
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  res.status(200).json(user);
+});
+
+app.get("/home", (req, res) => {
+  res.send("Welcome to the Home Page!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
